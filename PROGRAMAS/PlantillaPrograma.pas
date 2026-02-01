449ab0a35c08fd3a14e411938dfddcde32e74781
Zorrozoaga <103632157+Zorrozoaga@users.noreply.github.com>
2026-02-01 23:58:09 +0100

    Add files via upload

---
+++ PROGRAMAS/PlantillaPrograma.pas
@@ -0,0 +1,11 @@
+PROGRAM PlantillaPrograma;
+{$ASSERTIONS ON}
+VAR
+        {Declarar aqu¡ las variables}
+        X: INTEGER;
+BEGIN
+        {Codificar el programa aqu¡}
+        X := 1;
+        {Realizar aserciones aqu¡}
+        assert(X = 1);
+END.
